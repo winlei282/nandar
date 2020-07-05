1690bf677534dc09a75525f18469add5694c4511
winlei282 <67354529+winlei282@users.noreply.github.com>
2020-07-05 22:11:01 +0630

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hwconsole
+{
+    class Program
+    {
+        public static void Main()
+    {
+    Console.WriteLine("The current buffer height is {0} rows.",
+                      Console.BufferHeight);
+    Console.WriteLine("The current buffer width is {0} columns.",
+                      Console.BufferWidth);
+    }
+    }
+}
