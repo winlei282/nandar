444cf77ac0e6e613a46c977ce849f8a21e45d8d9
winlei282 <67354529+winlei282@users.noreply.github.com>
2020-07-05 22:26:51 +0630

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 
-namespace hwconsole
+namespace cancelkeypress
 {
     class Program
     {
         public static void Main()
     {
-    Console.WriteLine("The current buffer height is {0} rows.",
-                      Console.BufferHeight);
-    Console.WriteLine("The current buffer width is {0} columns.",
-                      Console.BufferWidth);
+        ConsoleKeyInfo cki;
+
+        Console.Clear();
+
+        // Establish an event handler to process key press events.
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(myHandler);
+        while (true)
+        {
+            Console.Write("Press any key, or 'X' to quit, or ");
+            Console.WriteLine("CTRL+C to interrupt the read operation:");
+
+            // Start a console read operation. Do not display the input.
+            cki = Console.ReadKey(true);
+
+            // Announce the name of the key that was pressed .
+            Console.WriteLine($"  Key pressed: {cki.Key}\n");
+
+            // Exit if the user pressed the 'X' key.
+            if (cki.Key == ConsoleKey.X) break;
+        }
+    }
+
+    protected static void myHandler(object sender, ConsoleCancelEventArgs args)
+    {
+        Console.WriteLine("\nThe read operation has been interrupted.");
+
+        Console.WriteLine($"  Key pressed: {args.SpecialKey}");
+
+        Console.WriteLine($"  Cancel property: {args.Cancel}");
+
+        // Set the Cancel property to true to prevent the process from terminating.
+        Console.WriteLine("Setting the Cancel property to true...");
+        args.Cancel = true;
+
+        // Announce the new value of the Cancel property.
+        Console.WriteLine($"  Cancel property: {args.Cancel}");
+        Console.WriteLine("The read operation will resume...\n");
     }
     }
 }
